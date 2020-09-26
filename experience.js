866c84528762c8bc135c40013bb2a68af7a513b7
Clara Nelson <claran704@gmail.com>
2020-09-26 17:18:47 -0400

    Adjusted formatting of experience toggle buttons. Adjusted initial load of experience page

--- experience.js
+++ experience.js
@@ -1,9 +1,22 @@
+function load_page() {
+	show_type();
+	// console.log($.get(navbar.html))
+    $.get("navbar.html", function(data) {
+    	$("#navbar").html(data);
+    	$('.navbar-nav').find('li:nth-child(3)').addClass('active');
+	})
+}
+
 function show_type() {
 	$("#bytype").show();
 	$("#byposition").hide();
+	$("#posButton").removeClass("active");
+	$("#typeButton").addClass("active");
 }
 
 function show_position() {
 	$("#bytype").hide();
 	$("#byposition").show();
+	$("#posButton").addClass("active");
+	$("#typeButton").removeClass("active");
 }
