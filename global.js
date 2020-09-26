6da42886bac30072b8ffa149e93bc671dac2589c
Clara Nelson <claran704@gmail.com>
2020-09-26 13:53:30 -0400

    Add new picture and heading spacing

---
+++ global.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+    //load navbar so it's consistent, based off (https://stackoverflow.com/questions/41916200/displaying-navbar-on-multiple-html-pages-using-bootstrap)
+    $("#navbar").load("navbar.html")
+
+      })
+  });
