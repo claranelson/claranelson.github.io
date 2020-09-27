fc0709dde7df7358d70c033ddfa865b32229bf0e
Clara Nelson <claran704@gmail.com>
2020-09-27 16:33:16 -0400

    minor text changes, remove global.css (unused)

--- global.js
+++ global.js
@@ -1,3 +1,4 @@
+//this function is not currently in use.
 $(document).ready(function(){
     //load navbar so it's consistent, based off (https://stackoverflow.com/questions/41916200/displaying-navbar-on-multiple-html-pages-using-bootstrap)
     $("#navbar").load("navbar.html")
