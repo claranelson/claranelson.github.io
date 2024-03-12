b3476723a1cfd0509ee1cc6ff2b3adebb0234664
Clara Nelson <claran@alumni.cmu.edu>
2024-03-11 21:19:12 -0400

    add margin to cardstack

--- src/components/CardStack.js
+++ src/components/CardStack.js
@@ -2,7 +2,7 @@ import classNames from 'classnames';
 export const CardStack = ({ children, className, ...props }) => {
 
   return (
-    <div className={classNames("d-flex gap-4 flex-column", className)} {...props}>
+    <div className={classNames("d-flex gap-4 flex-column mb-5", className)} {...props}>
       {children}
     </div>
   )
